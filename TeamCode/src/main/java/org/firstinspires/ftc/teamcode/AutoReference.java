d72645adffd5f553737de19535ca2e2ac6098a0f
IsaacTheAwesome <isaac.saff@gmail.com>
2021-02-01 16:56:36 -0500

    HAAAAAANDS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoReference.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+public class AutoReference {
+    public class UnderBridge {
+        public static final double leg1 = 0.25;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    public class FarBridge {
+        public static final double leg1 = 0.75;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    public class UnderBridgeEncoder {
+        public static final double leg1 = 0.25;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    public class FarBridgeEncoder {
+        public static final double leg1 = 0.75;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    public class BuildZoneGrabber{
+        public static final double leg1 = 0.75;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    //BrandonStuff
+
+}
